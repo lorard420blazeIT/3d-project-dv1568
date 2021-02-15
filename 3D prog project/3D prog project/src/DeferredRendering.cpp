b55c270d07a71aba21a90b6ce45d29a3262507a2
lorard <lorard14@gmail.com>
2021-02-15 11:54:06 +0100

    private members in deffered rendering

--- 3D prog project/3D prog project/src/DeferredRendering.cpp
+++ 3D prog project/3D prog project/src/DeferredRendering.cpp
@@ -4,7 +4,7 @@ void DeferredRendering::position()
 {
 }
 
-ID3D11Buffer DeferredRendering::gBuffer(ID3D11RenderTargetView gbufferRtv, ID3D11Texture2D texture2d, ID3D11ShaderResourceView shadersgbuffer)
-{
-    return ID3D11Buffer();
-}
+//ID3D11Buffer DeferredRendering::gBuffer(ID3D11RenderTargetView gbufferRtv, ID3D11Texture2D texture2d, ID3D11ShaderResourceView shadersgbuffer)
+//{
+//    return ID3D11Buffer();
+//}
--- 3D prog project/3D prog project/src/DeferredRendering.h
+++ 3D prog project/3D prog project/src/DeferredRendering.h
@@ -1,11 +1,18 @@
 #pragma once
+#include <d3d11.h>
+#include <string>
 class DeferredRendering
 {
 private:
 	
-
+	ID3D11Device* device;
+	ID3D11VertexShader*& vShader;
+	ID3D11PixelShader*& pShader;
+	std::string & vShaderByteCode;
+	ID3D11Texture2D*& texture;
+	std::string filePath;
 
 public:
 	void position();
-
+	/*ID3D11Buffer gBuffer(ID3D11RenderTargetView gbufferRtv, ID3D11Texture2D texture2d, ID3D11ShaderResourceView shadersgbuffer);*/
 };
