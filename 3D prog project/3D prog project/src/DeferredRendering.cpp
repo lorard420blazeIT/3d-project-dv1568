33c05aa7af3026af0511891be42d33498e64ce29
lorard <lorard14@gmail.com>
2021-02-15 11:53:31 +0100

    added private members in class defferd rendering

---
+++ 3D prog project/3D prog project/src/DeferredRendering.cpp
@@ -0,0 +1,10 @@
+#include "DeferredRendering.h"
+
+void DeferredRendering::position()
+{
+}
+
+ID3D11Buffer DeferredRendering::gBuffer(ID3D11RenderTargetView gbufferRtv, ID3D11Texture2D texture2d, ID3D11ShaderResourceView shadersgbuffer)
+{
+    return ID3D11Buffer();
+}
